e1458fa2dcae335932a480ecd96535cdd946a2b2
SagarTanwar896 <tanwarsagar896@gmail.com>
2025-10-18 16:06:06 +0530

    Update QuizGame.py

--- QuizGame.py
+++ QuizGame.py
@@ -1,5 +1,9 @@
 import streamlit as st
-st.balloons()
+# st.balloons()
 st.write("What is first letter of alphabet? \nA) b\tB) a\t\nC) s\tD) e")
 ans = st.text_input("Enter your answer....")
 st.write("you have entered....",ans)
+if ans == 'a':
+  st.ballons()
+else:
+  st.write("Better luck next time..")
