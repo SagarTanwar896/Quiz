ca4ff78a2de5c48449a1241ad6783bb8d17e157b
SagarTanwar896 <tanwarsagar896@gmail.com>
2025-11-01 13:14:52 +0530

    Update hhh.py

--- hhh.py
+++ hhh.py
@@ -1,1 +1,15 @@
-  
+import streamlit as st
+
+st.title("🎯 Quiz Game")
+st.write("Answer the following 5 questions:")
+
+st.write("ques1>what the natinal animal?")
+answer=st.text_input("enter the choice(tiger/cow/lion/horse):")
+st.write("ques2>what the natinal flower?")
+answer=st.text_input("enter the choice(lotus/sun flower/rose):")
+st.write("ques3>what the total bones?")
+answer=st.text_input("enter the choice(206/560/216/234):")
+st.write("ques4>what the friend name?")
+answer=st.text_input("enter the choice(vishal/monu/sagar/lokesh):")
+st.write("ques5>what the follwer in instragram?")
+answer=st.text_input("enter the choice(334/7657/57/876):")
